4b4f1f2c142c019cec62e2c16c4ed0fe4e0b2020
franciscohcf <franciscocavalcantefilho@gmail.com>
2022-03-16 14:55:03 -0300

    fix: removed remove_sand function

--- mine.rb
+++ mine.rb
@@ -1,22 +1,39 @@
 # frozen_string_literal: true
 
 class Mine
-  def remove_sand(soil)
-    soil.split('').reject { |element| element == ('.') }
+  def counts_diamonds(soil)
+    @diamonds = 0
+
+    soil = soil.split('').reject { |element| element == ('.') }
+
+    soil.each_with_index do |_e, i|
+      next unless soil[i] + soil[i + 1] == '<>'
+
+      @diamonds += 1
+
+      2.times do
+        soil.delete_at(i)
+      end
+    end
+
+    @diamonds
   end
 
-  def counts_diamonds(soil); end
-
   def counts_sand(soil)
-    @count = 0
+    @grains = 0
 
     soil = soil.split('')
 
     soil.each do |i|
-      @count += 1 if i == '.'
+      @grains += 1 if i == '.'
     end
 
-    @count
+    @grains
   end
 end
 
+soil = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
+
+mine = Mine.new
+
+print mine.counts_diamonds(soil)
